f6aeccb120b6ff7a9dc0d217bd0ce4451db29a9b
Ritiz <Ritiz@india.nec.com>
2023-06-02 11:31:16 +0530

    Added dto

---
+++ demo/src/main/java/com/experiment/bo/dto/EmployeeInfoDto.java
@@ -0,0 +1,19 @@
+package com.experiment.bo.dto;
+
+public class EmployeeInfoDto {
+
+	String employeeId;
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	String employeeName;
+}
